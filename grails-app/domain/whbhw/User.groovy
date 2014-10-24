7beae57394ae7dd0f6372889bdfe1246170a4ffa
AntoineWillems <willemsan.antoine@gmail.com>
2014-10-24 16:58:32 +0200

    Add user constraints Unit test: user constraints
    issue: #8

--- grails-app/domain/whbhw/User.groovy
+++ grails-app/domain/whbhw/User.groovy
@@ -10,6 +10,7 @@ class User {
     static constraints = {
         username size: 5..15, blank: false, unique: true
         password size: 5..15, blank: false
+        email email:true
         setup nullable:true
     }
 }
