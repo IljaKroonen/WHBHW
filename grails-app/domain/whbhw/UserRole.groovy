de5307bd85c4de2b048fa388328a3efb4ae7358a
Guillaume <guillaume.gurfinkiel@gmail.com>
2014-11-07 17:04:19 +0100

    #27 adding Role and UserRole

---
+++ grails-app/domain/whbhw/UserRole.groovy
@@ -0,0 +1,57 @@
+package whbhw
+
+import org.apache.commons.lang.builder.HashCodeBuilder
+
+class UserRole implements Serializable {
+
+    User secAppUser
+    Role secAppRole
+
+    boolean equals(other) {
+        if (!(other instanceof UserRole)) {
+            return false
+        }
+
+        other.secAppUser?.id == secAppUser?.id &&
+                other.secAppRole?.id == secAppRole?.id
+    }
+
+    int hashCode() {
+        def builder = new HashCodeBuilder()
+        if (User) builder.append(User.id)
+        if (Role) builder.append(Role.id)
+        builder.toHashCode()
+    }
+
+    static UserRole get(long secAppUserId, long secAppRoleId) {
+        find 'from UserRole where secAppUser.id=:secAppUserId and secAppRole.id=:secAppRoleId',
+                [secAppUserId: secAppUserId, secAppRoleId: secAppRoleId]
+    }
+
+    static UserRole create(User secAppUser, Role secAppRole, boolean flush = false) {
+        new UserRole(secAppUser: secAppUser, secAppRole: secAppRole).save(flush: flush, insert: true)
+    }
+
+    static boolean remove(User secAppUser, Role secAppRole, boolean flush = false) {
+        UserRole instance = UserRole.findBySecAppUserAndSecAppRole(secAppUser, secAppRole)
+        if (!instance) {
+            return false
+        }
+
+        instance.delete(flush: flush)
+        true
+    }
+
+    static void removeAll(User secAppUser) {
+        executeUpdate 'DELETE FROM UserRole WHERE secAppUser=:secAppUser', [secAppUser: secAppUser]
+    }
+
+    static void removeAll(Role secAppRole) {
+        executeUpdate 'DELETE FROM UserRole WHERE secAppRole=:secAppRole', [secAppRole: secAppRole]
+    }
+
+    static mapping = {
+        id composite: ['secAppRole', 'secAppUser']
+        version false
+    }
+}
